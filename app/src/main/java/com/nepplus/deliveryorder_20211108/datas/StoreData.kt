0d12409fbb473e78e1ae3601c61b9ceb14e7e2b7
김민성 <kmsz5813@naver.com>
2021-11-08 16:02:20 +0900

    리시트뷰 이벤트설정

--- app/src/main/java/com/nepplus/deliveryorder_20211108/datas/StoreData.kt
+++ app/src/main/java/com/nepplus/deliveryorder_20211108/datas/StoreData.kt
@@ -1,7 +1,9 @@
 package com.nepplus.deliveryorder_20211108.datas
 
+import java.io.Serializable
+
 class StoreData(
     val name: String,
     val phoneNum: String,
-    val logoURL: String) {
+    val logoURL: String) : Serializable {
 }
