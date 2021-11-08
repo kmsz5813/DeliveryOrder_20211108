d40c5e91b9fbda4432ea2eda72f7844e0087618f
김민성 <kmsz5813@naver.com>
2021-11-08 16:50:35 +0900

    닉네임 변경 누르면 -> 변경 화면으로 이동

---
+++ app/src/main/java/com/nepplus/deliveryorder_20211108/EditNicknameActivity.kt
@@ -0,0 +1,21 @@
+package com.nepplus.deliveryorder_20211108
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+    fun setValues() {
+
+    }
+
+}
